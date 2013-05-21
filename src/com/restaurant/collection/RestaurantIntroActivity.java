3ffa4b949f28eaf94b8e7f17421b7d07636d5de4
Steven Ko <chunyuko85@gmaill.com>
2013-05-21 18:23:49 +0800

    restaurant intro

--- src/com/restaurant/collection/RestaurantIntroActivity.java
+++ src/com/restaurant/collection/RestaurantIntroActivity.java
@@ -1,6 +1,7 @@
 package com.restaurant.collection;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -19,6 +21,10 @@ import com.viewpagerindicator.CirclePageIndicator;
 public class RestaurantIntroActivity extends SherlockFragmentActivity {
     private ViewPager pager;
 	private RelativeLayout watch_notes;
+	private ImageButton share_btn;
+	private ImageButton direction_button;
+	private ImageButton place_button;
+	private ImageButton favorite_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +55,45 @@ public class RestaurantIntroActivity extends SherlockFragmentActivity {
                 startActivity(intent);
             }
         });
+    	share_btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	Intent intent = new Intent(Intent.ACTION_SEND);
+            	intent.setType("text/plain");
+            	intent.putExtra(android.content.Intent.EXTRA_TEXT, "News for you!");
+            	startActivity(intent); 
+            }
+        });
+    	place_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	Uri uri = Uri.parse("geo:0,0?q=22.99948365856307,72.60040283203125(Maninagar)");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(intent);
+            }
+        });
+    	direction_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	Intent intent = new Intent(Intent.ACTION_VIEW,
+            			Uri.parse("http://maps.google.com/maps?saddr="+23.0094408+","+72.5988541+"&daddr="+22.99948365856307+","+72.60040283203125));
+            			startActivity(intent);
+            }
+        });
+    	favorite_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            	
+            }
+        });
 	}
 
 	private void findViews() {
 		watch_notes = (RelativeLayout)findViewById(R.id.watch_notes);
+		share_btn = (ImageButton)findViewById(R.id.share_button);
+		direction_button = (ImageButton)findViewById(R.id.direction_button);
+		place_button = (ImageButton)findViewById(R.id.place_button);
+		favorite_button = (ImageButton)findViewById(R.id.favorite_button);
 	}
 
 	class PhotoPagerAdapter extends FragmentPagerAdapter {
