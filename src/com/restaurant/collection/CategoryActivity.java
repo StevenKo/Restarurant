91fa087394be3028969738d787a155f2d5f97a82
Steven Ko <chunyuko85@gmaill.com>
2013-06-04 20:42:22 +0800

    restaurant sort

--- src/com/restaurant/collection/CategoryActivity.java
+++ src/com/restaurant/collection/CategoryActivity.java
@@ -3,17 +3,25 @@ package com.restaurant.collection;
 import java.util.ArrayList;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -28,14 +36,21 @@ import com.restaurant.fragment.AreaCategoryListFragment;
 import com.restaurant.fragment.CategoryTabFragment;
 import com.restaurant.fragment.SecondCategoryListFragment;
 import com.viewpagerindicator.TitlePageIndicator;
+import android.content.DialogInterface.OnClickListener;
 
-public class CategoryActivity extends SherlockFragmentActivity implements OnItemClickListener{
+interface AlertPositiveListener {
+    public void onPositiveClick(int position);
+}
+
+public class CategoryActivity extends SherlockFragmentActivity implements OnItemClickListener,AlertPositiveListener{
+	
 
     private static final int    ID_SETTING  = 0;
     private static final int    ID_RESPONSE = 1;
     private static final int    ID_ABOUT_US = 2;
     private static final int    ID_GRADE    = 3;
     private static final int    ID_MAP      = 4;
+    private static final int    ID_SORT     = 5;
     private AlertDialog.Builder aboutUsDialog;
     private ViewPager           pager;
 	private Bundle mBundle;
@@ -49,6 +64,7 @@ public class CategoryActivity extends SherlockFragmentActivity implements OnItem
 	private int rankCategoryId = 0;
 	private int typeId = 0;
 	private int secondCategoryId;
+	private int sortPosition = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -111,9 +127,83 @@ public class CategoryActivity extends SherlockFragmentActivity implements OnItem
         }
 
         setAboutUsDialog();
+        
     }
 
-    class AreaRankCategoryPagerAdapter extends FragmentPagerAdapter {
+    private void showPriceRangeDialog() {
+    	LayoutInflater inflater = this.getLayoutInflater();
+    	LinearLayout priceRangeLayout = (LinearLayout) inflater.inflate(R.layout.price_range_dialog,null);
+    	final EditText rangeStart = (EditText)priceRangeLayout.findViewById(R.id.price_range_start);
+    	final EditText rangeEnd = (EditText)priceRangeLayout.findViewById(R.id.price_range_end);
+    	
+    	Builder a = new AlertDialog.Builder(this).setTitle("選擇價格區間").setIcon(R.drawable.icon)
+                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    	
+
+                    }
+        }).setNegativeButton("取消", null);
+    	a.setView(priceRangeLayout);
+    	a.show();
+	}
+    
+    public class AlertDialogRadio  extends DialogFragment{
+    	
+    	AlertPositiveListener alertPositiveListener;
+    	final CharSequence[] items = {"距離 ","服務 ", "菜色", "價格 $0~200", "價格 $200~500", "價格$500~1000", "自訂價格"};
+    	
+    	public void onAttach(android.app.Activity activity) {
+            super.onAttach(activity);
+            try{
+                alertPositiveListener = (AlertPositiveListener) activity;
+            }catch(ClassCastException e){
+                // The hosting activity does not implemented the interface AlertPositiveListener
+                throw new ClassCastException(activity.toString() + " must implement AlertPositiveListener");
+            }
+        }
+    	
+    	OnClickListener positiveListener = new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                AlertDialog alert = (AlertDialog)dialog;
+                int position = alert.getListView().getCheckedItemPosition();
+                alertPositiveListener.onPositiveClick(position);
+            }
+        };
+        
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+     
+            /** Getting the arguments passed to this fragment */
+            Bundle bundle = getArguments();
+            int position = bundle.getInt("position");
+     
+            /** Creating a builder for the alert dialog window */
+            AlertDialog.Builder b = new AlertDialog.Builder(getActivity());
+     
+            /** Setting a title for the window */
+            b.setTitle("選擇排序方式");
+     
+            /** Setting items to the alert dialog */
+            b.setSingleChoiceItems(items, position, null);
+     
+            /** Setting a positive button and its listener */
+            b.setPositiveButton("確定",positiveListener);
+     
+            /** Setting a positive button and its listener */
+            b.setNegativeButton("取消", null);
+     
+            /** Creating the alert dialog window using the builder class */
+            AlertDialog d = b.create();
+     
+            /** Return the alert dialog window */
+            return d;
+        }
+    	
+    }
+
+	class AreaRankCategoryPagerAdapter extends FragmentPagerAdapter {
 
         ArrayList<Category> categories;
 
@@ -267,6 +357,7 @@ public class CategoryActivity extends SherlockFragmentActivity implements OnItem
         // menu.add(0, ID_SEARCH, 4, getResources().getString(R.string.menu_search)).setIcon(R.drawable.ic_search_inverse)
         // .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         menu.add(0, ID_MAP, 4, "地圖顯示餐廳").setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0, ID_SORT, 5, "排序").setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return true;
     }
 
@@ -293,8 +384,7 @@ public class CategoryActivity extends SherlockFragmentActivity implements OnItem
         	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.recommend_url)));
         	startActivity(browserIntent);
             break;
-        case ID_MAP:
-        	
+        case ID_MAP:	
             Intent intent = new Intent(CategoryActivity.this, MapActivity.class);
             Bundle bundle = new Bundle();
             bundle.putInt("AreaId", areaId);
@@ -303,6 +393,14 @@ public class CategoryActivity extends SherlockFragmentActivity implements OnItem
         	intent.putExtras(bundle);
             startActivity(intent);
             break;
+        case ID_SORT:
+        	android.app.FragmentManager manager = getFragmentManager();        	 
+            AlertDialogRadio alert = new AlertDialogRadio();
+            Bundle b  = new Bundle();
+            b.putInt("position", sortPosition );
+            alert.setArguments(b);
+            alert.show(manager, "alert_dialog_radio");
+        	break;
         }
         return true;
     }
@@ -322,5 +420,15 @@ public class CategoryActivity extends SherlockFragmentActivity implements OnItem
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
 		pager.setCurrentItem(position+2);
+	}
+
+	@Override
+	public void onPositiveClick(int position) {
+		sortPosition = position;
+        switch(position){
+        case 6:
+        	showPriceRangeDialog();
+        	break;
+        }
 	}
 }
